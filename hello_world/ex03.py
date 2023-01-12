05b57a5b88523487da7d2bb51defa2373afda9b5
Anton Simonov <asimonov215@gmail.com>
2023-01-12 16:31:22 +0300

    Nested loops and iterables

---
+++ hello_world/ex03.py
@@ -0,0 +1,12 @@
+# Nested loops
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
+
+# Iterables
+print(type(5))
+print(type(range(5)))
+# for x in range(5):
+# for x in "Python":
+for x in [1, 2, 3]:
+    print(x)

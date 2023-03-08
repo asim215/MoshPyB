3de880f69270a6960b718e629e7112fdccc92c7f
Anton Simonov <asimonov215@gmail.com>
2023-03-08 20:48:47 +0300

    Generator expressions

--- dev/data_structures/17_dict_comprehension.py
+++ dev/data_structures/17_dict_comprehension.py
@@ -15,6 +15,5 @@ values = {x * 2 for x in range(5)}
 print(values)
 
 # dicts
-
 values = {x: x * 2 for x in range(5)}
 print(values)
---
+++ dev/data_structures/18_generator_expressions.py
@@ -0,0 +1,13 @@
+from sys import getsizeof
+
+# Using List to store into memory
+values_l = [x**2 for x in range(2000)]
+for x in values_l:
+    print(x)
+# Using Generator Expressions - gen on the go
+values_g = (x**2 for x in range(2000))
+for x in values_g:
+    print(x)
+
+# size of obj
+print(f"Size of list {getsizeof(values_l)}, of generator object {getsizeof(values_g)}")

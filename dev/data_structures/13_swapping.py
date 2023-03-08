772155abbafe8961edfadcc7d00fcc2abe3b0722
Anton Simonov <asimonov215@gmail.com>
2023-03-08 17:43:28 +0300

    Swapping. Arrays

---
+++ dev/data_structures/13_swapping.py
@@ -0,0 +1,13 @@
+x = 10
+y = 11
+
+# z = x
+# x = y
+# y = z
+
+x, y = y, x
+# 1. define tuple (y, x)
+# 2. unpack tuple to x, y
+
+print("x = ", x)
+print("y = ", y)
---
+++ dev/data_structures/14_array.py
@@ -0,0 +1,10 @@
+from array import array
+
+numbers = array("i", [1, 2, 3])
+# type check at runtime
+# numbers[0] = 1.0
+
+# append
+# pop
+# insert
+# remove

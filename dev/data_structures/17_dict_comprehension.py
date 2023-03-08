c09b5065c88ebdb2a3a97377a6dbbda28d0109a1
Anton Simonov <asimonov215@gmail.com>
2023-03-08 20:28:38 +0300

    Lists, Sets, Dictionaries Comprehensions

---
+++ dev/data_structures/17_dict_comprehension.py
@@ -0,0 +1,20 @@
+values = []
+for x in range(5):
+    values.append(x * 2)
+# can be replaced with
+# map()
+# or
+# list comprehension
+print(values)
+
+# [expression for item in items]
+values = [x * 2 for x in range(5)]
+print(values)
+# set
+values = {x * 2 for x in range(5)}
+print(values)
+
+# dicts
+
+values = {x: x * 2 for x in range(5)}
+print(values)
